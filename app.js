2c3cdefcebcc181ce42172bb99fa7b70c4945681
Tina Nguyen <nguyentinaca@yahoo.com>
2021-02-12 23:22:02 -0800

    generate site with css

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const { profile } = require('console');
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -130,12 +129,20 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML)
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err)
 
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw err;
-
-            console.log('Portfolio complete! Check out index.html to see the output!');
-        });
     });
 
